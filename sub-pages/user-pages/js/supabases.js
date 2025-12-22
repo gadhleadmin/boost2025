4e8d75711815b9692e52931de68c7c60f9249265
ahmedgadhle4k <ahmadsom52@gmail.com>
2025-12-22 19:37:58 +0300

    first commit

--- sub-pages/user-pages/js/supabases.js
+++ sub-pages/user-pages/js/supabases.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = 'https://buzfscezwtuwwctckazm.supabase.co';
-const supabaseKey = 'sb_publishable_zyPtPoRPwhLw9RtdL9Oj6w_ARQpdqCl';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 // Bilaabidda Supabase client
 export const supabase = createClient(supabaseUrl, supabaseKey);
