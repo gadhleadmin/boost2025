346d75c2dcf479a9e631a03e22fdb50e66d019b0
ahmedgadhle4k <ahmadsom52@gmail.com>
2025-12-22 20:06:27 +0300

    first commit

--- sub-pages/user-pages/js/supabases.js
+++ sub-pages/user-pages/js/supabases.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = 'https://buzfscezwtuwwctckazm.supabase.co'; // Tusaale: process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1emZzY2V6d3R1d3djdGNrYXptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzNzY4ODksImV4cCI6MjA4MTk1Mjg4OX0.rIsOjDlwa_5B1jxeHEgnzZfmUaDGAra7jdgPJIlcbJo'; // Tusaale: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://iuxugjikgfoahpjayhma.supabase.co'; // Tusaale: process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1eHVnamlrZ2ZvYWhwamF5aG1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4ODIwMDAsImV4cCI6MjA4MTQ1ODAwMH0.Iyb5uSYxcKS3Ue6IloKBm-hMfBtHhWOdbxmZ2qLRp0U'; // Tusaale: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 // Bilaabidda (Initialization) macaamiisha Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
