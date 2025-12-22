a4c3165c90cd5ce7665bad0654a1714c471afbd6
ahmedgadhle4k <ahmadsom52@gmail.com>
2025-12-22 19:45:39 +0300

    first commit

--- sub-pages/user-pages/js/supabases.js
+++ sub-pages/user-pages/js/supabases.js
@@ -1,7 +1,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = 'https://buzfscezwtuwwctckazm.supabase.co';
-const supabaseKey = 'sb_publishable_zyPtPoRPwhLw9RtdL9Oj6w_ARQpdqCl';
+const supabaseUrl = 'https://buzfscezwtuwwctckazm.supabase.co'; // Tusaale: process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1emZzY2V6d3R1d3djdGNrYXptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzNzY4ODksImV4cCI6MjA4MTk1Mjg4OX0.rIsOjDlwa_5B1jxeHEgnzZfmUaDGAra7jdgPJIlcbJo'; // Tusaale: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-// Bilaabidda Supabase client
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// Bilaabidda (Initialization) macaamiisha Supabase
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
