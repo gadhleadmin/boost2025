bd0fd1fd80fbd3e82b3e0fab8148052150eee7ea
ahmedgadhle4k <ahmadsom52@gmail.com>
2025-12-23 20:35:01 +0300

    first commit

--- public/all_in_one/supabase.js
+++ public/all_in_one/supabase.js
@@ -2,10 +2,10 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 // URL sax ah ee mashruucaaga Supabase
-const supabaseUrl = 'https://buzfscezwtuwwctckazm.supabase.co';
+const supabaseUrl = 'https://vjdaylpouhhehtcjvukr.supabase.co';
 
 // Publishable (anon) API Key sax ah
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1emZzY2V6d3R1d3djdGNrYXptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzNzY4ODksImV4cCI6MjA4MTk1Mjg4OX0.rIsOjDlwa_5B1jxeHEgnzZfmUaDGAra7jdgPJIlcbJo';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqZGF5bHBvdWhoZWh0Y2p2dWtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0NTkzMTIsImV4cCI6MjA4MjAzNTMxMn0.qf8arJ_vFEFxUDF7bGIpDbaXlXqPYkOVfm-Vpq_zOLk';
 
 // Bilaabidda (Initialization) macaamiisha Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
